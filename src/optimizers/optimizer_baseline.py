3ff6843130b68ef669d52d3f96bfc5d18089d430
waverDeep <dandyleader@naver.com>
2021-10-13 16:42:41 +0900

    optimizer 코드 수정

--- src/optimizers/optimizer_baseline.py
+++ src/optimizers/optimizer_baseline.py
@@ -2,11 +2,14 @@ import torch.optim as optimizer
 import torch.optim.lr_scheduler as scheduler
 
 
-def get_optimizer(name, model_parameter, learning_rate, weight_decay, optimizer_param):
-    if name == 'Adam':
+def get_optimizer(model_parameter, optimizer_param):
+    if optimizer_param['optimizer_name'] == 'Adam':
         # We use the Adam optimizer [32] with a learning rate of 2e-4
-        return optimizer.Adam(params=model_parameter, lr=learning_rate, weight_decay=weight_decay,
-                              eps=optimizer_param['eps'], amsgrad=optimizer_param['amsgrad'],
+        return optimizer.Adam(params=model_parameter,
+                              lr=optimizer_param['learning_rate'],
+                              weight_decay=optimizer_param['weight_decay'],
+                              eps=optimizer_param['eps'],
+                              amsgrad=optimizer_param['amsgrad'],
                               betas=optimizer_param['betas'])
 
 
