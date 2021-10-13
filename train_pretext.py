d9390c91cf8145ab8a8a0fa951854ac6cbd30751
waverDeep <dandyleader@naver.com>
2021-10-13 16:42:05 +0900

    검증 코드 작성 완료 및 체크포인트 저장

--- train_pretext.py
+++ train_pretext.py
@@ -1,7 +1,7 @@
 import torch
 import argparse
 import json
-import time
+import os
 import numpy as np
 from tqdm import tqdm
 import src.data.dataset as dataset
@@ -73,14 +73,14 @@ def main():
     best_accuracy = 0
     # loss의 기본값을 무한대로 세팅
     best_loss = np.inf
-    best_epoch = -1
+    best_epoch = 0
 
-    format_logger.info("start training ...")
     num_of_epoch = config['train']['epoch']
     for epoch in range(num_of_epoch):
         epoch = epoch + 1
 
         # 학습 함수 시작
+        format_logger.info("start training ... [ {}/{} epoch ]".format(epoch, num_of_epoch))
         train(config=config,
               writer=writer,
               epoch=epoch,
@@ -88,6 +88,27 @@ def main():
               train_loader=train_loader,
               optimizer=optimizer,
               format_logger=format_logger)
+
+        # 검증 함수 시작
+        format_logger.info("start validating ... [ {}/{} epoch ]".format(epoch, num_of_epoch))
+        validation_accuracy, validation_loss = validation(config=config,
+                                                          writer=writer,
+                                                          epoch=epoch,
+                                                          model=model,
+                                                          validation_dataloader=validation_loader,
+                                                          format_logger=format_logger)
+
+        if validation_accuracy > best_accuracy:
+            best_accuracy = validation_accuracy
+            best_epoch = epoch
+            save_checkpoint(config=config,
+                            model=model,
+                            optimizer=optimizer,
+                            loss=validation_loss,
+                            epoch=best_epoch,
+                            format_logger=format_logger)
+
+
 
     # 텐서보드 종료
     tensorboard.close_tensorboard_writer(writer)
@@ -118,12 +139,52 @@ def train(config, writer, epoch, model, train_loader, optimizer, format_logger):
         writer.add_scalar('training accuracy', accuracy * 100, (epoch-1) * len(train_loader) + batch_idx)
 
 
-def validation(config, writer, epoch, model, validation_dataloader, optimizer, format_logger):
-    pass
+def validation(config, writer, epoch, model, validation_dataloader, format_logger):
+    model.eval()
+    total_loss = 0.0
+    total_accuracy = 0.0
+
+    with torch.no_grad():
+        validation_bar = tqdm(validation_dataloader,
+                              desc='{}/{} epoch validating ... '.format(epoch, config['train']['epoch']))
+        for batch_idx, data in enumerate(validation_bar):
+            # convolution 연산을 위채 1채널 추가
+            data = data.float().unsqueeze(1)
+            # GPU 환경 설정
+            if config['use_cuda']:
+                data = data.cuda()
+
+            hidden = model.init_hidden(len(data), config['use_cuda'])
+            accuracy, loss, hidden = model(data, hidden)
+
+            validation_bar.set_description('{}/{} epoch [ acc: {}/ loss: {} ]'.format(
+                                           epoch, config['train']['epoch'], round(float(accuracy), 3), round(float(loss), 3)))
+
+            # ...검증 중 손실(running loss)을 기록하고
+            writer.add_scalar('validating loss', loss, (epoch - 1) * len(validation_dataloader) + batch_idx)
+            writer.add_scalar('validating accuracy', accuracy * 100, (epoch - 1) * len(validation_dataloader) + batch_idx)
+
+            total_loss += len(data) * loss
+            total_accuracy += len(data) * accuracy
+
+        total_loss /= len(validation_dataloader.dataset)  # average loss
+        total_accuracy /= len(validation_dataloader.dataset)  # average acc
+
+        format_logger.info("[ {}/{} epoch validation result: [ average acc: {}/ average loss: {} ]".format(
+            epoch, config['train']['epoch'], total_accuracy, total_loss
+        ))
+
+    return total_accuracy, total_loss
 
 
-def save_checkpoint(config, ):
-    pass
+def save_checkpoint(config, model, optimizer, loss, epoch,format_logger):
+    file_path = os.path.join(config['checkpoint']['save_direcroy_path'],
+                             config['checkpoint']['file_name']+"-model_best.pt")
+    torch.save({
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "epoch": epoch, "loss": loss}, file_path)
+    format_logger.info("saved best checkpoint ... ")
 
 
 if __name__ == '__main__':
