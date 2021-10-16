291f0b92e895ce6c33b4ea6e89605c30ded58d9f
waverDeep <dandyleader@naver.com>
2021-10-16 14:59:53 +0900

    파일 입출력에 대한 함수들을 따로 관리할 수 있도록 파이썬파일로 분리

--- src/data/wav_to_hdf5.py
+++ src/data/wav_to_hdf5.py
@@ -3,6 +3,7 @@ import h5py
 import glob
 import os
 from tqdm import tqdm
+from src.utils import file_io_interface as io
 
 '''
 Hierachical Data Format version 5
@@ -15,28 +16,14 @@ Hierachical Data Format version 5
 '''
 
 
-def get_pure_filename(filename):
-    temp = filename.split('.')
-    del temp[-1]
-    temp = '.'.join(temp)
-    temp = temp.split('/')
-    temp = temp[-1]
-    return temp
-
-
-def get_all_file_path(input_dir, file_extension):
-    temp = glob.glob(os.path.join(input_dir, '**', '*.{}'.format(file_extension)), recursive=True)
-    return temp
-
-
 def wav_to_hdf5(directory_path_list, new_filepath_without_extension):
     h5_file = h5py.File('{}.h5'.format(new_filepath_without_extension), 'w')
     id_file = open('{}.txt'.format(new_filepath_without_extension), 'w')
     for directory_path in directory_path_list:
-        file_list = get_all_file_path(directory_path, 'flac')
+        file_list = io.get_all_file_path(directory_path, 'flac')
         for file in tqdm(file_list, desc=directory_path):
             waveform, sampling_rate = torchaudio.load(file)
-            filename = get_pure_filename(file)
+            filename = io.get_pure_filename(file)
             h5_file.create_dataset(filename, data=waveform)
             id_file.write('{}\n'.format(filename))
     h5_file.close()
@@ -48,14 +35,14 @@ if __name__ == '__main__':
                             '../dataset/LibriSpeech/train-clean-500']
     dev_directory_path = ['../dataset/LibriSpeech/dev-clean', '../dataset/LibriSpeech/dev-other']
     test_directory_path = ['../dataset/LibriSpeech/test-clean', '../dataset/LibriSpeech/test-other']
-    tiny_train_directory_path = ['../dataset/LibriSpeech/tiny-train']
-    tiny_dev_directory_path = ['../dataset/LibriSpeech/tiny-dev']
+    # tiny_train_directory_path = ['../dataset/LibriSpeech/tiny-train']
+    # tiny_dev_directory_path = ['../dataset/LibriSpeech/tiny-dev']
 
-    # wav_to_hdf5(train_directory_path, '../dataset/train-librispeech')
-    # wav_to_hdf5(dev_directory_path, '../dataset/dev-librispeech')
-    # wav_to_hdf5(test_directory_path, '../dataset/test-librispeech')
-    wav_to_hdf5(tiny_train_directory_path, '../dataset/tiny-train-librispeech')
-    wav_to_hdf5(tiny_dev_directory_path, '../dataset/tiny-dev-librispeech')
+    wav_to_hdf5(train_directory_path, '../dataset/train-librispeech')
+    wav_to_hdf5(dev_directory_path, '../dataset/dev-librispeech')
+    wav_to_hdf5(test_directory_path, '../dataset/test-librispeech')
+    # wav_to_hdf5(tiny_train_directory_path, '../dataset/tiny-train-librispeech')
+    # wav_to_hdf5(tiny_dev_directory_path, '../dataset/tiny-dev-librispeech')
 
 
 
