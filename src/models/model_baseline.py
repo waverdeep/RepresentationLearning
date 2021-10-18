05257220e6e3bee9b8da9bb21e4cc58f97c44dc2
waverDeep <dandyleader@naver.com>
2021-10-18 19:23:50 +0900

    수정된 InfoNCE 적용을 위해 코드 변경

--- src/models/model_baseline.py
+++ src/models/model_baseline.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn as nn
 import collections
 import numpy as np
+import src.losses.loss_baseline as criterion
 
 
-class CPC(nn.Module):
+class CPCType01(nn.Module):
     def __init__(self, timestep, audio_window):
-        super(CPC, self).__init__()
+        super(CPCType01, self).__init__()
         self.timestep = timestep
         self.audio_window = audio_window
         # We use five convolutional layers with strides [5, 4, 2, 2, 2],
@@ -124,6 +125,75 @@ class CPC(nn.Module):
         return accuracy, nce, hidden
 
 
+class CPCType02(nn.Module):
+    def __init__(self, args, g_enc_input, g_enc_hidden, g_ar_hidden, filter_sizes, strides, paddings):
+        super(CPCType02, self).__init__()
+        self.encoder = Encoder(input_dim=g_enc_input,
+                               hidden_dim=g_enc_hidden,
+                               filter_sizes=filter_sizes,
+                               strides=strides,
+                               paddings=paddings,
+                               )
+        self.autoregressive = AutoRegressive(input_dim=g_enc_hidden,
+                                             hidden_dim=g_ar_hidden,
+                                             )
+        self.loss = criterion.InfoNCE(args=args, gar_hidden=g_ar_hidden, genc_hidden=g_enc_hidden)
+
+    # pretext model에서 latent vector들을 추출하는 함수 작성함 (latent z, ct)
+    def get_latent_representations(self, x):
+        z = self.encoder(x)
+        # tensor.permute는 차원을 변경하는 함수이다. 현재는 L과 C 차원을 변경한다.
+        z = z.permute(0, 2, 1)
+        # autoregressive모델을 쳐서 만들어진 vector를 추출한다 -> context를 뜻
+        # 여기서 모든 timestep에 대한 c를 구해야하 하는지 좀 의문임
+        c = self.autoregressive(z)
+        return z, c
+
+    def forward(self, x):
+        z, c = self.get_latent_representations(x)
+        # loss 확인 필요
+        loss, accuracy = self.loss.get(x, z, c)
+        return loss, accuracy, z, c
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, strides, filter_sizes, paddings):
+        super(Encoder, self).__init__()
+        assert(
+                len(strides) == len(filter_sizes) == len(paddings)
+        ), "Inconsistent length of strides, filter sizes and padding"
+
+        self.encoder= nn.Sequential()
+        for index, (stride, filter_size, padding) in enumerate(zip(strides, filter_sizes, paddings)):
+            self.encoder.add_module(
+                "encoder_layer_{}".format(index),
+                nn.Sequential(
+                    nn.Conv1d(input_dim, hidden_dim, filter_size, stride=stride, padding=padding),
+                    nn.ReLU(),
+                )
+            )
+            input_dim = hidden_dim
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class AutoRegressive(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super(AutoRegressive, self).__init__()
+        self.autoregressive = nn.GRU(
+            input_dim,
+            hidden_dim,
+            batch_first=True,
+        )
+
+    def forward(self, x):
+        h0 = torch.zeros(1, x.size(0), self.hidden_dim).to(x.get_device())
+        self.autoregressive.flatten_parameters()
+        output, _ = self.autoregressive(x, h0)
+        return output
+
+
 def init_hidden(batch_size, use_gpu=True):
     if use_gpu:
         return torch.zeros(1, batch_size, 256).cuda()
@@ -132,13 +202,14 @@ def init_hidden(batch_size, use_gpu=True):
 
 
 if __name__ == '__main__':
-    CPC_model = CPC(12, 20480).cuda()
-    print(CPC_model.modules())
-    joy_data = torch.rand(8, 1, 20480).cuda()
-    accaracy, nce, hidden = CPC_model(joy_data, init_hidden(8))
-    print("acc: ", accaracy)
-    print("nce: ", nce)
-    print("hidden: ", hidden)
+    # CPC_model = CPCType01(12, 20480).cuda()
+    # print(CPC_model.modules())
+    # joy_data = torch.rand(8, 1, 20480).cuda()
+    # accaracy, nce, hidden = CPC_model(joy_data, init_hidden(8))
+    # print("acc: ", accaracy)
+    # print("nce: ", nce)
+    # print("hidden: ", hidden)
+    CPC_model = CPCType02()
 
 
 
--- src/utils/make_training_config.py
+++ src/utils/make_training_config.py
@@ -1,18 +1,20 @@
 import json
 
-configuration = {
-    "log_filename": "./log/training.log",
+name = 'direct_train02'
+
+configuration_type01 = {
+    "log_filename": "./log/{}.log".format(name),
     "dataset": {
-        "train_dataset": "./dataset/train-librispeech.h5",
-        "train_id_set": "./dataset/train-librispeech.txt",
-        "validation_dataset": "./dataset/dev-librispeech.h5",
-        "validation_id_set": "./dataset/dev-librispeech.txt",
+        "train_dataset": "./dataset/train-list-librispeech.txt",
+        # "train_id_set": "./dataset/train-librispeech.txt",
+        "validation_dataset": "./dataset/dev-list-librispeech.txt",
+        # "validation_id_set": "./dataset/dev-librispeech.txt",
         "num_workers": 8,
     },
     "parameter": {
         "timestep": 12,
-        "audio_window": 20840,
-        "batch_size": 64,
+        "audio_window": 20480,
+        "batch_size": 512,
     },
     "optimizer": {
         "optimizer_name": "Adam",
@@ -23,20 +25,79 @@ configuration = {
         "betas": (0.9, 0.98),
     },
     "train": {
-        "epoch": 50,
+        "epoch": 3000,
     },
     "tensorboard": {
-        "writer_name": "runs/test_space"
+        "writer_name": "runs/{}".format(name)
     },
     "checkpoint": {
         "save_directory_path": "./checkpoint",
-        "file_name": "test01",
+        "file_name": "{}".format(name),
     },
-    "use_cuda": False,
+    "use_cuda": True,
+}
+configuration = {
+    "log_filename": "./log/{}.log".format(name),
+    "dataset": {
+        "train_dataset": './dataset/train-list-librispeech.txt',
+        "validation_dataset": './dataset/dev-list-librispeech.txt',
+        "num_workers": 8,
+    },
+    "parameter": {
+        "timestep": 12,
+        "audio_window": 20480,
+        "batch_size": 512,
+    },
+    "optimizer": {
+        "optimizer_name": "Adam",
+        "learning_rate": 2e-4,
+        "weight_decay": 1e-4,
+        "eps": 1e-09,
+        "amsgrad": True,
+        "betas": (0.9, 0.98),
+    },
+    "train": {
+        "epoch": 3000,
+    },
+    "tensorboard": {
+        "writer_name": "runs/{}".format(name)
+    },
+    "checkpoint": {
+        "save_directory_path": "./checkpoint",
+        "file_name": "{}".format(name),
+    },
+    "use_cuda": True,
+}
+
+configuration_type02 = {
+    "log_filename": "./log/{}.log".format(name),
+    "dataset": {
+        "train_dataset": ".train-list-librispeech.txt",
+        # "train_id_set": "./dataset/train-librispeech.txt",
+        "validation_dataset": "dev-list-librispeech.txt",
+        # "validation_id_set": "./dataset/dev-librispeech.txt",
+        "num_workers": 8,
+    },
+    "tensorboard": {
+        "writer_name": "runs/{}".format(name)
+    },
+    "checkpoint": {
+        "save_directory_path": "./checkpoint",
+        "file_name": "{}".format(name),
+    },
+    "use_cuda": True,
+
 }
 
 if __name__ == '__main__':
-    filename = 'config_baseline.json'
-    with open('../../config/{}'.format(filename), 'w', encoding='utf-8') as config_file:
-        json.dump(configuration, config_file, indent='\t')
-    print('successfully created')
+    configuration_type = 1
+    if configuration_type == 1:
+        filename = 'config_type01_{}.json'.format(name)
+        with open('../../config/{}'.format(filename), 'w', encoding='utf-8') as config_file:
+            json.dump(configuration_type01, config_file, indent='\t')
+        print('successfully created')
+    elif configuration_type == 2:
+        filename = 'config_type02_{}.json'.format(name)
+        with open('../../config/{}'.format(filename), 'w', encoding='utf-8') as config_file:
+            json.dump(configuration_type02, config_file, indent='\t')
+        print('successfully created')
