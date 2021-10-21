f460b47312f1c27921d3b43528ee4c9af97a72a6
waverDeep <dandyleader@naver.com>
2021-10-21 14:59:41 +0900

    speaker classification 모델에 상용될 config파일 추가

--- src/utils/make_training_config.py
+++ src/utils/make_training_config.py
@@ -1,14 +1,12 @@
 import json
 
-name = 'direct_train10'
+name = 'direct_train01'
 
 configuration_type01 = {
     "log_filename": "./log/{}.log".format(name),
     "dataset": {
         "train_dataset": "./dataset/train-list-librispeech.txt",
-        # "train_id_set": "./dataset/train-librispeech.txt",
         "validation_dataset": "./dataset/dev-list-librispeech.txt",
-        # "validation_id_set": "./dataset/dev-librispeech.txt",
         "num_workers": 8,
     },
     "parameter": {
@@ -79,18 +77,80 @@ configuration_type02 = {
         "load_checkpoint": ''
     },
     "use_cuda": True,
+}
 
+configuration_speaker_classification = {
+    "log_filename": "./log/{}.log".format(name),
+    "dataset": {
+        "train_dataset": "./dataset/train-list-librispeech.txt",
+        "train_speaker_list": "./dataset/train-speaker-list-librispeech.txt",
+        "validation_dataset": "./dataset/dev-list-librispeech.txt",
+        "validation_speaker_list": "./dataset/dev-speaker-list-librispeech.txt",
+        "num_workers": 8,
+    },
+    "model": {
+        "batch_size": 3,
+    },
+    "pretext_model": {
+        "strides": [5, 4, 2, 2, 2],
+        "filter_sizes": [10, 8, 4, 4, 4],
+        "paddings": [2, 2, 2, 2, 1],
+        "g_enc_hidden": 512,
+        "g_ar_hidden": 256,
+        "g_enc_input": 1,
+        "load_checkpoint": './checkpoint/direct_train10-model_best.pt',
+    },
+    "downstream_model": {
+        "epoch": 100,
+        "speaker_num": 251,
+
+        "audio_window": 20480,
+    },
+    "optimizer": {
+        "optimizer_name": "Adam",
+        "learning_rate": 2e-4,
+        "weight_decay": 1e-4,
+        "eps": 1e-09,
+        "amsgrad": True,
+        "betas": (0.9, 0.98),
+    },
+    "loss": {
+      "learning_rate": 2.0e-4,
+      "prediction_step": 12, # Time steps k to predict into future
+      "negative_samples": 10, # Number of negative samples to be used for training
+      "subsample": True, # Boolean to decide whether to subsample from the total sequence lengh within intermediate layers
+      "calc_accuracy": True,
+    },
+    "tensorboard": {
+        "writer_name": "runs/{}".format(name)
+    },
+    "checkpoint": {
+        "save_directory_path": "./checkpoint",
+        "file_name": "{}".format(name),
+        "load_checkpoint": '',
+    },
+    "use_cuda": True,
 }
 
 if __name__ == '__main__':
-    configuration_type = 2
-    if configuration_type == 1:
-        filename = 'config_type01_{}.json'.format(name)
+    category = "spkcls"
+
+    if category == "spkcls":
+        filename = 'config_spkcls_{}.json'.format(name)
         with open('../../config/{}'.format(filename), 'w', encoding='utf-8') as config_file:
-            json.dump(configuration_type01, config_file, indent='\t')
-        print('successfully created')
-    elif configuration_type == 2:
-        filename = 'config_type02_{}.json'.format(name)
-        with open('../../config/{}'.format(filename), 'w', encoding='utf-8') as config_file:
-            json.dump(configuration_type02, config_file, indent='\t')
-        print('successfully created')
+            json.dump(configuration_speaker_classification, config_file, indent='\t')
+
+    elif category == "pretext":
+        configuration_type = 2
+        if configuration_type == 1:
+            filename = 'config_type01_{}.json'.format(name)
+            with open('../../config/{}'.format(filename), 'w', encoding='utf-8') as config_file:
+                json.dump(configuration_type01, config_file, indent='\t')
+            print('successfully created')
+        elif configuration_type == 2:
+            filename = 'config_type02_{}.json'.format(name)
+            with open('../../config/{}'.format(filename), 'w', encoding='utf-8') as config_file:
+                json.dump(configuration_type02, config_file, indent='\t')
+            print('successfully created')
+
+
