cafe5d6a3945f6e550cc9bab66b4df327f3aaedc
waverDeep <dandyleader@naver.com>
2021-10-23 22:55:59 +0900

    speaker classification 학습 코드 수정

--- train_downsteam_spkcls.py
+++ train_downsteam_spkcls.py
@@ -9,10 +9,12 @@ import src.utils.logger as logger
 import src.models.model_baseline as model_baseline
 import src.optimizers.optimizer_baseline as optimizer_baseline
 import src.utils.setup_tensorboard as tensorboard
+import src.utils.setup_criterion as criterion
 from apex.parallel import DistributedDataParallel as DDP
 import torch.backends.cudnn as cudnn
 import torch.distributed as distributed
 from src.models import model_downstream
+from sklearn.metrics import accuracy_score
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
@@ -55,7 +57,7 @@ def main():
         description='pytorch representation learning downstream task - speaker classification')
     # DISTRIBUTED 사용하기 위해서는 local rank를 argument로 받아야함. 그러면 torch.distributed.launch에서 알아서 해줌
     parser.add_argument("--local_rank", default=0, type=int)
-    parser.add_argument('--configuration', required=False, default='./config/config_spkcls_direct_train01.json')
+    parser.add_argument('--configuration', required=False, default='./config/config_spkcls_direct_train02.json')
     args = parser.parse_args()
 
     # 학습에 필요한 모든 configuration 및 파라미터들을 json으로 불러오기
@@ -112,7 +114,7 @@ def main():
     if config['pretext_model']['load_checkpoint'] != '':
         if config['use_cuda'] == True:
             checkpoint = torch.load(config['pretext_model']['load_checkpoint'], map_location="cuda:0")
-            pretext_model.load_state_dict(checkpoint['model_state_dict'])
+            pretext_model.load_state_dict(checkpoint['model_module_state_dict'])
         else:
             device = torch.device('cpu')
             checkpoint = torch.load(config['pretext_model']['load_checkpoint'], map_location=device)
@@ -121,7 +123,7 @@ def main():
     # downstream task에 대한 checkpoint 불러오기 (있으면)
     if config['checkpoint']['load_checkpoint'] != '':
         if config['use_cuda'] == True:
-            checkpoint = torch.load(config['checkpoint']['load_checkpoint'])
+            checkpoint = torch.load(config['checkpoint']['load_checkpoint'], map_location="cuda:0")
             pretext_model.load_state_dict(checkpoint['model_state_dict'])
         else:
             device = torch.device('cpu')
@@ -152,6 +154,9 @@ def main():
     optimizer = optimizer_baseline.get_optimizer(model_parameter=downstream_model.parameters(),
                                                  optimizer_param=config['optimizer'])
 
+    # loss 생성
+    loss_function = criterion.set_criterion("NLLLoss")
+
     # 학습 코드 작성
     best_accuracy = 0
 
@@ -168,6 +173,7 @@ def main():
               downstream_model=downstream_model,
               train_loader=train_loader,
               optimizer=optimizer,
+              loss_function=loss_function,
               format_logger=format_logger)
 
         # 검증 함수 시작
@@ -177,6 +183,7 @@ def main():
                                                           epoch=epoch,
                                                           pretext_model=pretext_model,
                                                           downstream_model=downstream_model,
+                                                          loss_function=loss_function,
                                                           validation_dataloader=validation_loader,
                                                           format_logger=format_logger)
 
@@ -204,11 +211,12 @@ def main():
     tensorboard.close_tensorboard_writer(writer)
 
 
-def train(config, writer, epoch, pretext_model, downstream_model, train_loader, optimizer, format_logger):
+def train(config, writer, epoch, pretext_model, downstream_model, train_loader, optimizer, loss_function, format_logger):
     pretext_model.eval() # freeze pretrain model parameters
     downstream_model.train()
 
-    criterion = nn.CrossEntropyLoss()
+    total_loss = 0.0
+    total_accuracy = 0.0
 
     for batch_idx, [data, target] in enumerate(train_loader):
         # convolution 연산을 위채 1채널 추가
@@ -219,42 +227,48 @@ def train(config, writer, epoch, pretext_model, downstream_model, train_loader, 
         if config['use_cuda']:
             data = data.cuda()
             target = target.cuda()
-        # gru 모델에 들어간 hidden state 설정하기
+        # pretext model : gru 모델에 들어간 hidden state 설정하기
         # format_logger.info("pred_model ... ")
-        loss, accuracy, z, c = pretext_model(data)
-        # print("context size : ", c.size())
-
-        data = c[:, -1, :]
-        print("data size : ", data.size())
-        # target = target.view((-1,))
-
-        output = downstream_model(data)
-        print("output : ", output.size())
-        print("target : ", target)
-        pred = output.max(1,)[1]  # get the index of the max log-probability
-        print("pred : ", pred)
-        print("target : ", target.size())
-        print("pred : ", pred.size())
-
+        with torch.no_grad():
+            p_loss, p_accuracy, z, c = pretext_model(data)
+        # z -> latent vector
+        # c -> context vector : 나는 가장 마지막에 추출된 context vector를 사용해서 classification을
+        #                       진행할 예정
+        _data = c[:, -1, :]
+        output = downstream_model(_data)
+        loss = loss_function(output, target)
         optimizer.zero_grad()
-        loss = criterion(pred, target)
         loss.backward()
         optimizer.step()
+        pred = output.max(1)[1].cpu()
+        target = target.cpu()
+        accuracy = accuracy_score(target, pred)
+
+        writer.add_scalar('Loss/batch-train', round(float(loss.item()), 3), (epoch - 1) * len(train_loader) + batch_idx)
+        writer.add_scalar('Accuracy/batch-train', round(float(accuracy * 100), 3), (epoch - 1) * len(train_loader) + batch_idx)
+
+        total_loss += len(data) * loss.item()
+        total_accuracy += len(data) * accuracy
+
+        linear = 0
+        for idx, layer in enumerate(downstream_model.modules()):
+            if isinstance(layer, torch.nn.Linear):
+                writer.add_histogram("Linear/weights-{}".format(linear), layer.weight,
+                                     global_step=(epoch - 1) * len(train_loader) + batch_idx)
+                writer.add_histogram("Linear/bias-{}".format(linear), layer.bias,
+                                     global_step=(epoch - 1) * len(train_loader) + batch_idx)
+                linear += 1
+
+    total_loss /= len(train_loader.dataset)  # average loss
+    total_accuracy /= len(train_loader.dataset)  # average acc
+
+    writer.add_scalar('Loss/average-train', round(float(total_loss), 3), (epoch - 1) * len(train_loader) + batch_idx)
+    writer.add_scalar('Accuracy/average-train', round(float(total_accuracy * 100), 3), (epoch - 1) * len(train_loader) + batch_idx)
 
 
-        accuracy = 1. * pred.eq(target.view_as(pred)).sum().item() / len(data)
-
-        # format_logger.info("write_tensorboard ... ")
-        # ...학습 중 손실(running loss)을 기록하고
-        writer.add_scalar('Loss/train', loss, (epoch - 1) * len(train_loader) + batch_idx)
-        writer.add_scalar('Accuracy/train', accuracy * 100, (epoch - 1) * len(train_loader) + batch_idx)
-
-
-def validation(config, writer, epoch, pretext_model, downstream_model, validation_dataloader, format_logger):
+def validation(config, writer, epoch, pretext_model, downstream_model, validation_dataloader, loss_function, format_logger):
     pretext_model.eval()
     downstream_model.eval()
-
-    criterion = nn.NLLLoss()
 
     total_loss = 0.0
     total_accuracy = 0.0
@@ -273,25 +287,26 @@ def validation(config, writer, epoch, pretext_model, downstream_model, validatio
             # format_logger.info("pred_model ... ")
             loss, accuracy, z, c = pretext_model(data)
 
-            data = c.contiguous().view((-1, 256))
-            target = target.view((-1, 1))
+            _data = c[:, -1, :]
+            output = downstream_model(_data)
+            loss = loss_function(output, target)
 
-            output = downstream_model(data)
-
-            loss = criterion(output, target)
-
-            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
-            accuracy = 1. * pred.eq(target.view_as(pred)).sum().item() / len(data)
+            pred = output.max(1)[1].cpu()
+            target = target.cpu()
+            accuracy = accuracy_score(target, pred)
 
             # ...검증 중 손실(running loss)을 기록하고
-            writer.add_scalar('Loss/validate', loss, (epoch - 1) * len(validation_dataloader) + batch_idx)
-            writer.add_scalar('Accuracy/validate', accuracy * 100, (epoch - 1) * len(validation_dataloader) + batch_idx)
+            writer.add_scalar('Loss/batch-validate', loss.item(), (epoch - 1) * len(validation_dataloader) + batch_idx)
+            writer.add_scalar('Accuracy/batch-validate', accuracy * 100, (epoch - 1) * len(validation_dataloader) + batch_idx)
 
-            total_loss += loss
-            total_accuracy += accuracy
+            total_loss += len(data) * loss.item()
+            total_accuracy += len(data) * accuracy
 
         total_loss /= len(validation_dataloader.dataset)  # average loss
         total_accuracy /= len(validation_dataloader.dataset)  # average acc
+
+        writer.add_scalar('Loss/average-validate', round(float(total_loss), 3), (epoch - 1) * len(validation_dataloader) + batch_idx)
+        writer.add_scalar('Accuracy/average-validate', round(float(total_accuracy * 100), 3), (epoch - 1) * len(validation_dataloader) + batch_idx)
 
         format_logger.info("[ {}/{} epoch validation result: [ average acc: {}/ average loss: {} ]".format(
             epoch, config['downstream_model']['epoch'], total_accuracy, total_loss
