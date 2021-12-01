8810b113100f30d9014a29b03a07123f3652670d
waverDeep <dandyleader@naver.com>
2021-12-01 22:25:39 +0900

    json Load 기능 추가와 directory 생성 기능 추가

--- src/utils/file_io_interface.py
+++ src/utils/file_io_interface.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import json
 
 
 def get_pure_filename(filename):
@@ -14,3 +15,20 @@ def get_pure_filename(filename):
 def get_all_file_path(input_dir, file_extension):
     temp = glob.glob(os.path.join(input_dir, '**', '*.{}'.format(file_extension)), recursive=True)
     return temp
+
+
+def load_json_config(filename):
+    with open(filename, 'r') as configuration:
+        config = json.load(configuration)
+    return config
+
+
+def make_directory(directory_name, format_logger=None):
+    try:
+        if not os.path.exists(directory_name):
+            os.makedirs(directory_name)
+    except OSError:
+        if format_logger is not None:
+            format_logger.info('Error: make directory: {}'.format(directory_name))
+        else:
+            print('Error: make directory: {}'.format(directory_name))
