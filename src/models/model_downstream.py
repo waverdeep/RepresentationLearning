2f293bb324e9c5f75f4bd1aa83aed75c1f897135
waverDeep <dandyleader@naver.com>
2021-12-03 12:53:14 +0900

    speaker classification 모델을 구조 수정

--- src/models/model_downstream.py
+++ src/models/model_downstream.py
@@ -6,20 +6,19 @@ class SpeakerClassification(nn.Module):
     def __init__(self, hidden_dim, speaker_num):
         super(SpeakerClassification, self).__init__()
         self.speaker_num = speaker_num
-
+        self.adaptive_avg_pool1d = nn.AdaptiveAvgPool1d(1)
         self.classifier = nn.Sequential(
             collections.OrderedDict(
                 [
                     ('linear01', nn.Linear(hidden_dim, speaker_num))
-                    # ('linear01', nn.Linear(256, 512)),
-                    # ('bn01', nn.BatchNorm1d(512)),
-                    # ('relu01', nn.ReLU()),
-                    # ('linear02', nn.Linear(512, self.speaker_num)),
                 ]
             )
         )
-        self.softmax = nn.Softmax()
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x = self.adaptive_avg_pool1d(x)
+        x = x.permute(0, 2, 1).reshape(-1, 256)
         output = self.classifier(x)
         return self.softmax(output)
