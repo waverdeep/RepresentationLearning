4cf6674d5d05d423fd06396e67fd3283e5aa1210
waverDeep <dandyleader@naver.com>
2021-12-03 12:52:53 +0900

    speaker classification 모델을 불러올 수 있는 코드 추가

--- src/models/model.py
+++ src/models/model.py
@@ -1,8 +1,9 @@
 import src.models.model_baseline as model_baseline
+import src.models.model_downstream as model_downstream
+import torch
 
 
-def load_model(config):
-    model_name = config['model_type']
+def load_model(config, model_name, checkpoint_path=None):
     model = None
     if model_name == 'CPCModel':
         model = model_baseline.CPCModel(
@@ -14,5 +15,16 @@ def load_model(config):
             strides=config['strides'],
             paddings=config['paddings'],
         )
+    elif model_name == 'SpeakerClassification':
+        model = model_downstream.SpeakerClassification(
+            hidden_dim=config['g_ar_hidden'],
+            speaker_num=config['speaker_num']
+        )
+
+    if checkpoint_path is not None:
+        device = torch.device('cpu')
+        checkpoint = torch.load(checkpoint_path, map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+
     return model
 
