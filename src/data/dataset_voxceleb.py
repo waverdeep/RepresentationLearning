c0e53895b640d68b387de5f4cf68a3a8915ff64f
waverDeep <dandyleader@naver.com>
2021-12-15 13:10:44 +0900

    voxceleb 데이터셋을 로딩하기 위한 데이터로더 구현

---
+++ src/data/dataset_voxceleb.py
@@ -0,0 +1,35 @@
+import src.data.dataset_tool_speaker as speaker_tool
+import src.utils.interface_audio_io as audio_io
+
+
+def get_vox_speaker_list(file_list):
+    speaker_list = []
+    for index, file in enumerate(file_list):
+        temp = file.split('/')
+        speaker_id = temp[5][2:]
+        speaker_list.append(speaker_id)
+    return speaker_list
+
+
+class VoxWaveformDataset(speaker_tool.NormalWaveformDataset):
+    def __init__(self, directory_path, audio_window=20480, full_audio=False):
+        speaker_tool.NormalWaveformDataset.__init__(self, directory_path, audio_window)
+        self.audio_window = audio_window
+        self.full_audio = full_audio
+        self.speaker_list = get_vox_speaker_list(self.file_list)
+        self.speaker_dict = speaker_tool.get_speaker_dict(self.speaker_list)
+
+    def __getitem__(self, index):
+        # ../../dataset/vox01/wav/id10977/radm0JQM9aI/00012.wav
+        audio_file = self.file_list[index]
+        temp = audio_file.split('/')
+        speaker_id = temp[5][2:]
+        audio_file = audio_file[4:]
+        waveform, sampling_rate = audio_io.audio_loader("{}".format(audio_file))
+        # sampling rate가 16000가 아니면 에러 메시지를 띄워줄 수 있도록 함
+        assert (
+                sampling_rate == 16000
+        ), "sampling rate is not consistent throughout the dataset"
+        if not self.full_audio:
+            waveform = speaker_tool.random_cutoff(waveform, self.audio_window)
+        return waveform, 0, speaker_id
