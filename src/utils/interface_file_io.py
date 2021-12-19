bf525c52fbc2d2c95ea8cb6efd1f7be0e3f03605
waverDeep <dandyleader@naver.com>
2021-12-19 14:27:20 +0900

    csv 읽기 기능 추가

--- src/utils/interface_file_io.py
+++ src/utils/interface_file_io.py
@@ -1,6 +1,16 @@
 import glob
 import os
 import json
+import csv
+
+
+def read_csv_file(filename):
+    dataset = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = csv.reader(file)
+        for line in data:
+            dataset.append(line)
+    return dataset
 
 
 def get_pure_filename(filename):
@@ -49,6 +59,24 @@ def make_list2txt(file_list, file_path):
 def list_divider(step, data):
     split_len = int(len(data)/step)
     return [data[i:i+split_len] for i in range(0, len(data), split_len)]
+
+
+if __name__ == '__main__':
+    name = "concat"
+    if name == "concat":
+        train = list()
+        kspon_train = read_txt2list("../../dataset/train-kspon-20480.txt")
+        sr_train = read_txt2list("../../dataset/speaker_recognition-train-20480.txt")
+        train.extend(kspon_train)
+        train.extend(sr_train)
+        make_list2txt(train, "../../dataset/sr_and_ks-train-20480.txt")
+
+        dev = list()
+        kspon_dev = read_txt2list("../../dataset/test-kspon-20480.txt")
+        sr_dev = read_txt2list("../../dataset/speaker_recognition-dev-20480.txt")
+        dev.extend(kspon_dev)
+        dev.extend(sr_dev)
+        make_list2txt(dev, "../../dataset/sr_and_ks-dev-20480.txt")
 
 
 
