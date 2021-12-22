ae1c8f87cda1d6094e4fd62c3a9bad7212941f36
waverDeep <dandyleader@naver.com>
2021-12-22 14:51:04 +0900

    pretrained model을 사용할 수 있도록 구조 설계

--- src/models/model.py
+++ src/models/model.py
@@ -1,5 +1,7 @@
 import src.models.model_baseline as model_baseline
 import src.models.model_downstream as model_downstream
+import src.models.model_custom as model_custom
+import src.models.model_proposed01 as model_proposed01
 import torch
 
 
@@ -15,10 +17,51 @@ def load_model(config, model_name, checkpoint_path=None):
             strides=config['strides'],
             paddings=config['paddings'],
         )
+    elif model_name == 'GenerativeCPCModel':
+        model = model_proposed01.GenerativeCPCModel(
+            args=config,
+            g_enc_input=config['g_enc_input'],
+            g_enc_hidden=config['g_enc_hidden'],
+            g_ar_hidden=config['g_ar_hidden'],
+            filter_sizes=config['filter_sizes'],
+            strides=config['strides'],
+            paddings=config['paddings'],
+        )
     elif model_name == 'SpeakerClassification':
         model = model_downstream.SpeakerClassification(
             hidden_dim=config['g_ar_hidden'],
             speaker_num=config['speaker_num']
+        )
+    elif model_name == "PhonemeClassification":
+        model = model_downstream.PhonemeClassification(
+            hidden_dim=config['g_ar_hidden'],
+            speaker_num=config['num_classes']
+        )
+    elif model_name == 'ResnetCombineModel':
+        model = model_custom.ResNetCombine(
+            config=config,
+            embedding_size=config['embedding_size'],
+            num_classes=config['speaker_num'],
+            backbone='resnet18',
+            pretrained=False,
+        )
+    elif model_name == 'VGGCombineModel':
+        model = model_custom.VGGCombine(
+            config=config,
+            embedding_size=config['embedding_size'],
+            num_classes=config['speaker_num']
+        )
+    elif model_name == 'MobileNetV2CombineModel':
+        model = model_custom.MobileNetV2Combine(
+            config=config,
+            embedding_size=config['embedding_size'],
+            num_classes=config['speaker_num']
+        )
+    elif model_name == 'SqueezeNetCombineModel':
+        model = model_custom.SqueezeNetCombine(
+            config=config,
+            embedding_size=config['embedding_size'],
+            num_classes=config['speaker_num']
         )
 
     if checkpoint_path is not None:
