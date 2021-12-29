d08281ab60ef36470fbf5a2a8860a6e5c692a64e
waverDeep <dandyleader@naver.com>
2021-12-29 14:57:49 +0900

    librosa library를 사용해서 mel spectrogram을 추출하는 코드 추가

--- src/utils/interface_audio_io.py
+++ src/utils/interface_audio_io.py
@@ -84,6 +84,17 @@ def distributed_pcm2wav(pcm_file):
     print("end data distribution...")
 
 
+class MelSpectrogramLibrosa:
+    """Mel spectrogram using librosa."""
+    def __init__(self, fs=16000, n_fft=1024, shift=160, n_mels=64, fmin=60, fmax=7800):
+        self.fs, self.n_fft, self.shift, self.n_mels, self.fmin, self.fmax = fs, n_fft, shift, n_mels, fmin, fmax
+        self.mfb = librosa.filters.mel(sr=fs, n_fft=n_fft, n_mels=n_mels, fmin=fmin, fmax=fmax)
+
+    def __call__(self, audio):
+        X = librosa.stft(np.array(audio), n_fft=self.n_fft, hop_length=self.shift)
+        return torch.tensor(np.matmul(self.mfb, np.abs(X) ** 2 + np.finfo(float).eps))
+
+
 if __name__ == '__main__':
     task = ""
     if task == "resampling":
